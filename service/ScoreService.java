e5b54e6af893bdfe79fec88e6bf1588e274c5df4
gwengopi <gwengopi.gg@gmail.com>
2024-03-16 03:44:37 +0530

    Refactored score service with impl layer

--- service/ScoreService.java
+++ service/ScoreService.java
@@ -2,8 +2,6 @@ package home.projects.cricket.score.service;
 
 import home.projects.cricket.score.dto.ScoreDTO;
 
-public class ScoreService {
-    public void logScore(ScoreDTO scoreDTO) {
-
-    }
+public interface ScoreService {
+    void logScore(ScoreDTO scoreDTO);
 }
