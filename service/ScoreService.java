f550a900afcfd127934441a96fff1a949a9d5009
gwengopi <gwengopi.gg@gmail.com>
2024-03-15 09:54:43 +0530

    Basic service class with simple implementation

---
+++ service/ScoreService.java
@@ -0,0 +1,9 @@
+package home.projects.cricket.score.service;
+
+import home.projects.cricket.score.dto.ScoreDTO;
+
+public class ScoreService {
+    public void logScore(ScoreDTO scoreDTO) {
+
+    }
+}
