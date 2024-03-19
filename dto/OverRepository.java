e3c1db71a356515c8c040e41ee28344a6b15be9e
gwengopi <gwengopi.gg@gmail.com>
2024-03-19 19:59:29 +0530

    moved overrepository to dto to repo package

--- dto/OverRepository.java
+++
@@ -1,9 +0,0 @@
-package home.projects.cricket.score.dto;
-
-import home.projects.cricket.score.entity.Over;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface OverRepository extends MongoRepository<Over, String> {
-    Over findByMatchIdAndTeam(String matchId, String team);
-}
-
